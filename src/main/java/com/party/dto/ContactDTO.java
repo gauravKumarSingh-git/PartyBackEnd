27bc2ad3a22fc1de902d66d2c99dd48b871109b4
Amar Khedkar <smartynew0@gmail.com>
2023-02-14 23:00:34 +0530

    Update ContactDTO.java

--- src/main/java/com/party/dto/ContactDTO.java
+++ src/main/java/com/party/dto/ContactDTO.java
@@ -1,5 +1,47 @@
 package com.party.dto;
 
+import java.util.*;
 public class ContactDTO {
 
+    private String name;
+	private String description;
+
+    public ContactDTO() {}
+	public ContactDTO( String name , String description) {
+		super();
+		this.name = name;
+		this.description = description;     
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+		this.name = name;   
+	}   
+    public String getdescription() {
+        return description;
+    }
+    public void setdescription(String description) {
+		this.description = description;
+    }
+    @Override
+	public int hashCode() {
+		return Objects.hash( description, name);
+	}
+    @Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContactDTO other = (ContactDTO) obj;
+		return Objects.equals(description, other.description) && Objects.equals(name, other.name);
+    }
+	@Override
+	public String toString() {
+        return  "Contact [name=" + name + ", description=" + description + "]";
+	}
 }
