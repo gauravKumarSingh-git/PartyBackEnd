0eac2d29e17d3f6ab1c5a68157b1f74c11880d66
Amar Khedkar <smartynew0@gmail.com>
2023-02-15 00:01:36 +0530

    .

--- src/main/java/com/party/service/ContactServiceImpl.java
+++ src/main/java/com/party/service/ContactServiceImpl.java
@@ -1,5 +1,55 @@
 package com.party.service;
 
-public class ContactServiceImpl {
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.party.dto.ContactDTO;
+import com.party.entity.Contact;
+import com.party.exception.PartyException;
+import com.party.repository.ContactRepository;
+
+@Service
+public class ContactServiceImpl implements ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public String addContact(ContactDTO contactDTO) throws PartyException {
+        Contact contact = modelMapper.map(contactDTO, Contact.class);
+        contactRepository.save(contact);
+        return "Contact added successfully";
+    }
+
+    @Override
+    public String updateContact(ContactDTO contactDTO) throws PartyException {
+        Contact existingContact = contactRepository.findById(contactDTO.getContactId())
+                .orElseThrow(() -> new PartyException("Contact not found"));
+        modelMapper.map(contactDTO, existingContact);
+        contactRepository.save(existingContact);
+        return "Contact updated successfully";
+    }
+
+    @Override
+    public void deleteContact(int contactId) throws PartyException {
+        if (!contactRepository.existsById(contactId)) {
+            throw new PartyException("Contact not found");
+        }
+        contactRepository.deleteById(contactId);
+    }
+
+    @Override
+    public List<ContactDTO> getContacts() throws PartyException {
+        List<Contact> contacts = contactRepository.findAll();
+        return contacts.stream()
+                .map(contact -> modelMapper.map(contact, ContactDTO.class))
+                .collect(Collectors.toList());
+    }
 }
