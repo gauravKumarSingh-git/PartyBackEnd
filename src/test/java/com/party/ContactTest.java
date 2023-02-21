ea33fed753df3b92b82ddd82d0abfe130594a9da
Amar Khedkar <smartynew0@gmail.com>
2023-02-21 14:16:00 +0530

    Update ContactTest.java

--- src/test/java/com/party/ContactTest.java
+++ src/test/java/com/party/ContactTest.java
@@ -10,7 +10,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.party.dto.ContactDTO;
